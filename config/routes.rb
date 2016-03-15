1e2ac3def4c042a0000f48f5257952c40c1156e4
sophie watine <sophiewatine@gmail.com>
2016-03-15 18:24:47 +0100

    navbar and profile update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'meals#index'
 
-  resources :profiles, only: :show
+  resource :profile, only: [:show, :edit, :update]
   resources :users do
-   resources :profiles, only: [:show, :edit, :update]
+   #resources :profiles, only: [:show, :edit, :update]
    resources :meals, only: [:new, :create, :edit, :update]
   end
 
