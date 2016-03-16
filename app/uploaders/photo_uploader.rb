31a82a9e1b27eda6d54e71aeea1157760fe3ec56
Nicolas Mulliez <mulliez.nicolas@gmail.com>
2016-03-16 11:33:46 +0100

    carrierwave, mini-mgic fog et s3

---
+++ app/uploaders/photo_uploader.rb
@@ -0,0 +1,13 @@
+class PhotoUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick # <- Notice this
+
+  storage :fog                    # <- and this
+
+  version :standard do
+    resize_to_fill 400, 300
+  end
+
+  version :thumb do
+    resize_to_fill 100, 100
+  end
+end
